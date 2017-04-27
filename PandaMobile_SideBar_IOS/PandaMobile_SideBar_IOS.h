10f7a36fa8b4e14e406cf41408b7e371c4fdc852
lingen.liu <lingen.liu@gmail.com>
2017-04-27 12:26:50 +0800

    Initial Commit

---
+++ PandaMobile_SideBar_IOS/PandaMobile_SideBar_IOS.h
@@ -0,0 +1,19 @@
+//
+//  PandaMobile_SideBar_IOS.h
+//  PandaMobile_SideBar_IOS
+//
+//  Created by 刘林 on 2017/4/27.
+//  Copyright © 2017年 刘林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PandaMobile_SideBar_IOS.
+FOUNDATION_EXPORT double PandaMobile_SideBar_IOSVersionNumber;
+
+//! Project version string for PandaMobile_SideBar_IOS.
+FOUNDATION_EXPORT const unsigned char PandaMobile_SideBar_IOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PandaMobile_SideBar_IOS/PublicHeader.h>
+
+
